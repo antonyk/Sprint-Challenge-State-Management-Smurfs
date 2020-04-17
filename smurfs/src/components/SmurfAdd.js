a58b8c8e07260787da25ecfd531886f561795d0b
Anthony K <antonyk@users.noreply.github.com>
2020-04-17 14:24:29 -0400

    fixed form state tracking

--- smurfs/src/components/SmurfAdd.js
+++ smurfs/src/components/SmurfAdd.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { postSmurf } from '../store/smurfActions';
 
 function SmurfAdd(props) {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(null);
+  const [height, setHeight] = useState('');
+
+  const [formData, setFormData] = useState({})
 
   function addSmurf(e) {
     e.preventDefault();
-    const data = new FormData(e.target);
-    // console.log({data})
-    props.postSmurf(data)
+    // const data = new FormData(e.target);
+    console.log('data: ',formData)
+    props.postSmurf(formData)
+  }
+
+  function formChangeHandler(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+    // console.log(formData)
   }
 
   return (
     <>
-      <form className='add-item-form' onSubmit={addSmurf}>
+      <form className='add-item-form' onSubmit={(e) => addSmurf(e)}>
         <label className='form-item'>Name 
-          <input name='name' type='text' />
+          <input name='name' type='text' onChange={formChangeHandler} />
         </label>
         <label className='form-item'>Age 
-          <input name='age' type='text' />
+          <input name='age' type='text' onChange={formChangeHandler} />
         </label>
         <label className='form-item'>Height 
-          <input name='height' type='text' />
+          <input name='height' type='text' onChange={formChangeHandler} />
         </label>
         <button className='form-button' >Add</button>
       </form>
